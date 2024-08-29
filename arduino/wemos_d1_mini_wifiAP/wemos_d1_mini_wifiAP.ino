f1da2f2aecc8bef414fd4a20572b1e5392a3e4ee
Sergey Ilyin <sergey.ilyin@stockholmstream.com>
2024-08-29 16:17:49 +0200

    Arduino AP

---
+++ arduino/wemos_d1_mini_wifiAP/wemos_d1_mini_wifiAP.ino
@@ -0,0 +1,241 @@
+// Board esp8266 -> LOLIN(WEMOS) D1 R2 & mini
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <ESP8266mDNS.h>
+
+const char *apSSID = "WemosD1Mini";
+const char *apPassword = "12341234";
+const char *DomainName = "wemos";  // wemos.local
+
+bool ledState = 0;
+const int ledPin = 2;
+
+IPAddress apIP(192, 168, 0, 1);       // 192.168.4.22
+IPAddress apGateway(192, 168, 0, 1);  // 192.168.4.9
+IPAddress apSubnet(255, 255, 255, 0);
+
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <title>ESP Web Server</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+  html {
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+  }
+  h1 {
+    font-size: 1.8rem;
+    color: white;
+  }
+  h2{
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: #143642;
+  }
+  .topnav {
+    overflow: hidden;
+    background-color: #143642;
+  }
+  body {
+    margin: 0;
+  }
+  .content {
+    padding: 30px;
+    max-width: 600px;
+    margin: 0 auto;
+  }
+  .card {
+    background-color: #F8F7F9;;
+    box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);
+    padding-top:10px;
+    padding-bottom:20px;
+  }
+  .button {
+    padding: 15px 50px;
+    font-size: 24px;
+    text-align: center;
+    outline: none;
+    color: #fff;
+    background-color: #0f8b8d;
+    border: none;
+    border-radius: 5px;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+   }
+   /*.button:hover {background-color: #0f8b8d}*/
+   .button:active {
+     background-color: #0f8b8d;
+     box-shadow: 2 2px #CDCDCD;
+     transform: translateY(2px);
+   }
+   .state {
+     font-size: 1.5rem;
+     color:#8c8c8c;
+     font-weight: bold;
+   }
+  </style>
+<title>ESP Web Server</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="data:,">
+</head>
+<body>
+  <div class="topnav">
+    <h1>ESP WebSocket Server</h1>
+  </div>
+  <div class="content">
+    <div class="card">
+      <h2>Output - GPIO 2</h2>
+      <p class="state">state: <span id="state">%STATE%</span></p>
+      <p><button id="button" class="button">Toggle</button></p>
+    </div>
+  </div>
+<script>
+  var gateway = `ws://${window.location.hostname}/ws`;
+  var websocket;
+  window.addEventListener('load', onLoad);
+  function initWebSocket() {
+    console.log('Trying to open a WebSocket connection...');
+    websocket = new WebSocket(gateway);
+    websocket.onopen    = onOpen;
+    websocket.onclose   = onClose;
+    websocket.onmessage = onMessage; // <-- add this line
+  }
+  function onOpen(event) {
+    console.log('Connection opened');
+  }
+  function onClose(event) {
+    console.log('Connection closed');
+    setTimeout(initWebSocket, 2000);
+  }
+  function onMessage(event) {
+    var state;
+    if (event.data == "1"){
+      state = "ON";
+    }
+    else{
+      state = "OFF";
+    }
+    document.getElementById('state').innerHTML = state;
+  }
+  function onLoad(event) {
+    initWebSocket();
+    initButton();
+  }
+  function initButton() {
+    document.getElementById('button').addEventListener('click', toggle);
+  }
+  function toggle(){
+    websocket.send('toggle');
+  }
+</script>
+</body>
+</html>
+)rawliteral";
+
+void notifyClients() {
+  ws.textAll(String(ledState));
+}
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo *)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    data[len] = 0;
+    Serial.printf("WebSocket client sent data %s\n", (char *)data);
+    if (strcmp((char *)data, "toggle") == 0) {
+      ledState = !ledState;
+      notifyClients();
+    }
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
+             void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+}
+
+// replace the %STATE% placeholder with ON if the ledState is 1. Otherwise, replace it with OFF
+String processor(const String &var) {
+  if (var == "STATE") {
+    String result = ledState ? "ON" : "OFF";
+    Serial.printf("Replacing %s by %s\n", var, result);
+    return result;
+  }
+  return String();
+}
+
+void startMDNS() {
+  if (!MDNS.begin(DomainName, WiFi.softAPIP())) {
+    Serial.println("[ERROR] MDNS responder did not setup");
+    while (1) {
+      delay(1000);
+    }
+  } else {
+    Serial.println("MDNS setup is successful!");
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  delay(6000);
+  //while (!Serial.available()) {}
+
+  Serial.print("Setting soft-AP configuration ... ");
+  Serial.println(
+    WiFi.softAPConfig(apIP, apGateway, apSubnet)
+      ? "Ready"
+      : "Failed!");
+
+  Serial.print("Setting soft-AP ... ");
+  Serial.println(
+    WiFi.softAP(apSSID, apPassword)
+      ? "Ready"
+      : "Failed!");
+
+  IPAddress localIP = WiFi.softAPIP();
+  Serial.print("AP IP Address: ");
+  Serial.println(localIP);
+
+  // startMDNS();
+
+  initWebSocket();
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  server.begin();
+}
+
+void loop() {
+  //Serial.print(".");
+  //delay(100);
+  ws.cleanupClients(10);
+  // MDNS.update();
+}
